10055d85eea56db9c31b195657e65f69243f7c64
michael-canaran <michael.canaran@ryerson.ca>
2020-09-04 02:02:28 -0400

    Clean navbar and fix routes.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,25 +7,23 @@ function Navbar(props) {
     return (
         <div className="header">
             <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
-                <a className="navbar-brand brand" href="#">WebPOS</a>
+                <Link to="/" className="navbar-brand brand">WebPOS</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarsExampleDefault">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <Link to="/" className="nav-link" href="#">Terminal <span className="sr-only">(current)</span></Link>
                         </li>
-                        <li className="nav-item dropdown nav-item active">
-                            <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Inventory</a>
-                            <div className="dropdown-menu" aria-labelledby="dropdown01">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                            </div>
+                        <li className="nav-item active">
+                            <Link to="/inventory" className="nav-link" href="#">Inventory <span className="sr-only">(current)</span></Link>
                         </li>
+                        {/* <li className="nav-item active">
+                            <a className="nav-link" href="#">Customers <span className="sr-only">(current)</span></a>
+                        </li> */}
                     </ul>
-                    <button type="button" className="btn btn-outline-danger btn-lg">Logoff</button>
+                    <Link to="/logout"><button type="button" className="btn btn-outline-danger btn-lg">Logout</button></Link>
                 </div>
             </nav>
         </div>
