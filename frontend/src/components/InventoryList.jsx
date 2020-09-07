e48fe2aa67c8eb95fe615e1152165bdf797a763e
michael-canaran <michael.canaran@ryerson.ca>
2020-09-07 02:12:24 -0400

    Render items dynamically as they are added.

--- frontend/src/components/InventoryList.jsx
+++ frontend/src/components/InventoryList.jsx
@@ -1,33 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+import Item from './Item';
 
 import "../styles/inventory-list.css"
 
-function InventoryList(props) {
+function InventoryList() {
+    const [inventory, setInventory] = useState([]);
+    
+    function getItems() {
+        Axios.get('http://localhost:5000/inventory')
+            .then(res => setInventory(res.data))
+            .catch((err) => console.log('Error: '+err));
+    }
+    getItems();
     return (
         <div className="container-fluid">
             <div className="inventory-list-container">
                 <div className="heading">Inventory</div>
                     <br/>
-                    <div className="row">
-                        <div className="col">
-                        Item Number
+                    <div className="row column-heading">
+                        <div className="col-sm">
+                            Item Number
                         </div>
-                        <div className="col">
-                        Item Name
+                        <div className="col-sm">
+                            Item Name
                         </div>
-                        <div className="col">
-                        Description
+                        <div className="col-sm">
+                            Description
                         </div>
-                        <div className="col">
-                        Stock
+                        <div className="col-sm">
+                            Stock
                         </div>
-                        <div className="col">
-                        Price
+                        <div className="col-sm">
+                            Price
+                        </div>
                     </div>
-                </div>
+                    {inventory.map((item, index) => {
+                        return (
+                            <Item
+                                key={index}
+                                itemNumber={item.itemNumber}
+                                itemName={item.itemName}
+                                description={item.description}
+                                stock={item.stock}
+                                price={item.price}
+                            />
+                        )
+                    })}
+                <div>
             </div>
         </div>
+    </div>
     );
 }
 
 export default InventoryList;
+
+// {items.map((item, index) => {
+//     return (
+//         <Item
+//             key={index}
+//             itemNumber={item.itemNumber}
+//             itemName={item.itemName}
+//             description={item.description}
+//             stock={item.stock}
+//             price={item.price}
+//             onDelete={deleteItem}
+//         />
+//     )
+// })}
