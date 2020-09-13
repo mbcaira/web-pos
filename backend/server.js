8d21e8633ee56cf53aac45dd390512574b0bce5f
michael-canaran <michael.canaran@ryerson.ca>
2020-09-13 00:59:13 -0400

    Edit procfile.

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join('./build')));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join('./build/index.html'))
 })
 
 const port = process.env.PORT || 5000;
