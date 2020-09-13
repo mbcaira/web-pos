adfc31303f04ced964321a62f676a34d03ac7c9b
michael-canaran <michael.canaran@ryerson.ca>
2020-09-13 00:55:17 -0400

    Edit procfile.

---
+++ backend/build/precache-manifest.57ca7201471a4ffa852a5cbcbb31e5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ea2cb43219839796994f18f0248e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b345a7ba01f5eb4d26",
+    "url": "/static/css/main.65c38dfe.chunk.css"
+  },
+  {
+    "revision": "2e2c5afd635a6eae5d1f",
+    "url": "/static/js/2.ff1a72ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff1a72ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b345a7ba01f5eb4d26",
+    "url": "/static/js/main.ce104441.chunk.js"
+  },
+  {
+    "revision": "040d1ba22c438ef801c0",
+    "url": "/static/js/runtime-main.edcae4df.js"
+  }
+]);
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
